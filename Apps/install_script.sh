5aab524d3c8fda3506e9db4885ba7294f6b2751b
touste <touste@users.noreply.github.com>
2021-11-06 15:32:00 +0100

    Update install_script.sh

--- Apps/install_script.sh
+++ Apps/install_script.sh
@@ -9,10 +9,12 @@ git clone https://github.com/agkozak/zsh-z.git $HOME/.zprezto-contrib/zsh-z
 
 zsh -i -c 'git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"'
 
+zsh -i -c '
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
+'
 
 chsh -s $(which zsh)
 
