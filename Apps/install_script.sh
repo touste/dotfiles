aba8d3d117f6f288025fccc665e0722d8f247d9a
master <master@pop-os.localdomain>
2021-11-06 15:21:09 +0100

    Add install script

---
+++ Apps/install_script.sh
@@ -0,0 +1,47 @@
+# Apps dir
+echo "Making apps dir"
+mkdir -p $HOME/Apps
+
+# Prezto
+echo "Installing Prezto for zsh"
+zsh -i -c 'git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"'
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+chsh -s $(which zsh)
+
+
+# Get micro
+echo "Installing micro editor"
+cd .local/bin
+curl https://getmic.ro | bash
+cd $HOME
+
+# Add .local/bin to path
+echo "Adding .local/bin to path"
+if [[ ":$PATH:" == *":$HOME/.local/bin:"* ]]; then
+  echo ".local/bin already in path"
+else
+  echo "export PATH=$HOME/.local/bin:$PATH" >> $HOME/.zshrc
+fi
+
+# Configure dotfile repo (https://www.atlassian.com/git/tutorials/dotfiles)
+echo "Importing dotfiles..."
+
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.zshrc
+
+git clone --bare <git-repo-url> $HOME/.cfg
+
+mkdir -p .config/config-backup && \
+dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | \
+xargs -I{} mv {} .config/config-backup/{}
+
+dotfiles checkout
+
+dotfiles config --local status.showUntrackedFiles no
+
+# z jump
